1b3682fd340564edc32401876710999aa5a027ac
mrquangquynh <mrquangquynh@gmai.com>
2013-05-09 01:31:08 +0700

    Update install-bigbluebutton0.80-dev.sh

--- bbb-deploy/install-bigbluebutton0.80-dev.sh
+++ bbb-deploy/install-bigbluebutton0.80-dev.sh
@@ -37,7 +37,7 @@ sudo mv ~/Script_install_video/bbb-dev0.80/conf ~/dev/bigbluebutton/bigbluebutto
 sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/lib.old
 sudo mv ~/Script_install_video/bbb-dev0.80/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/
 sudo mv ~/Script_install_video/bbb-dev0.80/LayoutModule.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/
-sudo mv ~/Script_install_video/bbb-dev0.80/client.html ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/Script_install_video/bbb-dev0.80/video.html ~/dev/bigbluebutton/bigbluebutton-client/bin/
 sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/BigBlueButton.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/video.swf
 
 
