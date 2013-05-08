64dcf699d7a3059b4800ba3e5e638b080fc1afaf
mrquangquynh <mrquangquynh@gmai.com>
2013-05-08 17:30:07 +0700

    Update install-bigbluebutton0.80-dev.sh

--- bbb-deploy/install-bigbluebutton0.80-dev.sh
+++ bbb-deploy/install-bigbluebutton0.80-dev.sh
@@ -1,6 +1,83 @@
 #!/bin/bash
 
+sudo video-conf --setup-dev tools
+
+source ~/.profile
+
+cd ~/dev
+
+git clone https://github.com/quangquynh-bbb081/bigbluebutton
+
+cd ~/dev/bigbluebutton
+
+git status
+
+git checkout -b my-bbb-branch v0.8
+
+git status
+
+cd ../..
+bbb-conf --setup-dev client
+
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/src ~/dev/bigbluebutton/bigbluebutton-client/src.old
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/locale ~/dev/bigbluebutton/bigbluebutton-client/locale.old
+sudo mv ~/Script_install_video/bbb-dev0.80/src ~/dev/bigbluebutton/bigbluebutton-client/
+sudo mv ~/Script_install_video/bbb-dev0.80/locale ~/dev/bigbluebutton/bigbluebutton-client/
+
+
+cd ~/dev/bigbluebutton/bigbluebutton-client
+ant
+ant locales
+
+sudo mv ~/Script_install_video/bbb-dev0.80/branding ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/conf.old
+sudo mv ~/Script_install_video/bbb-dev0.80/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/lib.old
+sudo mv ~/Script_install_video/bbb-dev0.80/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/Script_install_video/bbb-dev0.80/LayoutModule.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/Script_install_video/bbb-dev0.80/client.html ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/BigBlueButton.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/video.swf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 sudo apt-get -y install openjdk-6-jdk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # https://github.com/harrah/xsbt/wiki/Setup
 mkdir -p ~/tools
