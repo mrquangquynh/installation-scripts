ad48b28df0c955d321f1393ca9b29b82c6ad2845
mrquangquynh <mrquangquynh@gmai.com>
2013-05-09 01:40:03 +0700

    Create install-bigbluebutton0.80-dev_part1.sh

---
+++ bbb-deploy/install-bigbluebutton0.80-dev_part1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo video-conf --setup-dev client
+
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/src ~/dev/bigbluebutton/bigbluebutton-client/src.old
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/locale ~/dev/bigbluebutton/bigbluebutton-client/locale.old
+sudo mv ~/Script_install_video/bbb-dev0.80/src ~/dev/bigbluebutton/bigbluebutton-client/
+sudo mv ~/Script_install_video/bbb-dev0.80/locale ~/dev/bigbluebutton/bigbluebutton-client/
