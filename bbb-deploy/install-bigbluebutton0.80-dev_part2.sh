30cac7b7013ca4a504ba979c27e0435219da5cdd
mrquangquynh <mrquangquynh@gmai.com>
2013-05-09 09:50:12 +0700

    Update install-bigbluebutton0.80-dev_part2.sh

--- bbb-deploy/install-bigbluebutton0.80-dev_part2.sh
+++ bbb-deploy/install-bigbluebutton0.80-dev_part2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "#-------------------------Thay doi giao dien ben Devlopment-----------------------"
+
 sudo mv ~/Script_install_video/bbb-dev0.80/branding ~/dev/bigbluebutton/bigbluebutton-client/bin/
 sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/conf.old
 sudo mv ~/Script_install_video/bbb-dev0.80/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/
@@ -8,3 +10,11 @@ sudo mv ~/Script_install_video/bbb-dev0.80/lib ~/dev/bigbluebutton/bigbluebutton
 sudo mv ~/Script_install_video/bbb-dev0.80/LayoutModule.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/
 sudo mv ~/Script_install_video/bbb-dev0.80/video.html ~/dev/bigbluebutton/bigbluebutton-client/bin/
 sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/BigBlueButton.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/video.swf
+
+
+
+echo "#-------------------------Doi client --> bkippbx (ben Devlopment)-----------------------"
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/client ~/dev/bigbluebutton/bigbluebutton-client/bkippbx
+sudo mv /etc/bigbluebutton/nginx/client_dev /etc/bigbluebutton/nginx/client_dev.old
+sudo mv ~/Script_install_video/bbb-dev0.80/client_dev /etc/bigbluebutton/nginx/
+sudo chmod 755 /etc/bigbluebutton/nginx/client_dev
