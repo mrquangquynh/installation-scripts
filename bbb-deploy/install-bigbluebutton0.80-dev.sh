dec6e82533fd4e44f0de4f969661505fb57c51fa
mrquangquynh <mrquangquynh@gmai.com>
2013-05-13 09:49:11 +0700

    Update install-bigbluebutton0.80-dev.sh

--- bbb-deploy/install-bigbluebutton0.80-dev.sh
+++ bbb-deploy/install-bigbluebutton0.80-dev.sh
@@ -1,57 +1,69 @@
-Cac buoc cai dat BBB0.8 DEV
 
-sudo video-conf --setup-dev tools
+
+#!/bin/bash
+
+
+#Cac buoc cai dat BBB0.8 DEV
+
+bbb-conf --setup-dev tools
+
+echo "------------source ~/.profile-----------------"
 
 source ~/.profile
 
+echo "------------git clone github.com/.../.../-----------------"
+
+
 cd ~/dev
 
-git clone https://github.com/quangquynh-bbb081/bigbluebutton
+git clone https://github.com/bigbluebutton/bigbluebutton
+
+echo "------------git status - checkout -----------------"
 
 cd ~/dev/bigbluebutton
 
-git status
-
 git checkout -b my-bbb-branch v0.8
 
-git status
 
-cd 
-sudo video-conf --setup-dev client
+bbb-conf --setup-dev client
 
-wget https://raw.github.com/mrquangquynh/installation-scripts/master/bbb-deploy/install-bigbluebutton0.80-dev_part1.sh
-chmod +x install-bigbluebutton0.80-dev_part1.sh
-./install-bigbluebutton0.80-dev_part1.sh
+echo "#-------------------------Thay doi code Devlopment-----------------------"
 
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/src ~/dev/bigbluebutton/bigbluebutton-client/src.old
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/locale ~/dev/bigbluebutton/bigbluebutton-client/locale.old
+sudo mv ~/Script_install_video/bbb-dev0.80/src ~/dev/bigbluebutton/bigbluebutton-client/
+sudo mv ~/Script_install_video/bbb-dev0.80/locale ~/dev/bigbluebutton/bigbluebutton-client/
+
+
+echo "------------Build Flash -----------------"
 
 cd ~/dev/bigbluebutton/bigbluebutton-client
+
 ant
+
+echo "------------Build langue -----------------"
+
 ant locales
 
-cd 
 
-wget https://raw.github.com/mrquangquynh/installation-scripts/master/bbb-deploy/install-bigbluebutton0.80-dev_part2.sh
-chmod +x install-bigbluebutton0.80-dev_part2.sh
-./chmod +x install-bigbluebutton0.80-dev_part2.sh
+echo "#-------------------------Thay doi giao dien ben Devlopment-----------------------"
 
-
-sudo video-conf --setip IP or HOST:PORT
-
-#Chuyển về BBB thường dùng   :
-#              $   sudo ln -s -f /etc/bigbluebutton/nginx/client /etc/bigbluebutton/nginx/client.nginx
-#              $   sudo /etc/init.d/nginx restart     
-#Chuyển về BBB-dev : 
-#              $  sudo ln -s -f /etc/bigbluebutton/nginx/client_dev /etc/bigbluebutton/nginx/client.nginx
-#              $  sudo /etc/init.d/nginx restart  
+sudo mv ~/Script_install_video/bbb-dev0.80/branding ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/conf.old
+sudo cp -r ~/Script_install_video/bbb-dev0.80/conf ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/lib.old
+sudo mv ~/Script_install_video/bbb-dev0.80/lib ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/Script_install_video/bbb-dev0.80/LayoutModule.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/Script_install_video/bbb-dev0.80/video.html ~/dev/bigbluebutton/bigbluebutton-client/bin/
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/bin/BigBlueButton.swf ~/dev/bigbluebutton/bigbluebutton-client/bin/video.swf
 
 
 
-
-
-
-
-
-
+echo "#-------------------------Doi client --> bkippbx (ben Devlopment)-----------------------"
+sudo mv ~/dev/bigbluebutton/bigbluebutton-client/client ~/dev/bigbluebutton/bigbluebutton-client/bkippbx
+sudo mv /etc/bigbluebutton/nginx/client_dev /etc/bigbluebutton/nginx/client_dev.old
+sudo mv ~/Script_install_video/bbb-dev0.80/client_dev /etc/bigbluebutton/nginx/
+sudo chmod 755 /etc/bigbluebutton/nginx/client_dev
 
 
 
