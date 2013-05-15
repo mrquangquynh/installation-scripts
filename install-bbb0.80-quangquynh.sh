7e088587497fe6b8ce9437f35372405e8ef99771
mrquangquynh <mrquangquynh@gmai.com>
2013-05-15 16:04:49 +0700

    Update install-bbb0.80-quangquynh.sh

--- install-bbb0.80-quangquynh.sh
+++ install-bbb0.80-quangquynh.sh
@@ -25,9 +25,8 @@ then
 	print_usage
 fi
 
-
+sudo apt-get -y update
 echo "Updating Unzip"
-
 
 sudo apt-get install unzip
 
@@ -46,7 +45,7 @@ sudo cp -r ~/download_bbb32-64b/var_lib_apt_lists/* /var/lib/apt/lists/
 
 
 echo "Updating the Ubuntu package repository"
-sudo apt-get -y update
+
 sudo apt-get install bmon
 sudo apt-get -y install mysql-server
 sudo apt-get -y install git-core htop iftop ant curl
